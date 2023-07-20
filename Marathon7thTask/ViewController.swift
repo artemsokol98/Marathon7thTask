612e4a58898a1a8f4b10340058f9195aa82e297d
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-07-21 00:53:30 +0300

    add stretch image with scrollView

--- Marathon7thTask/ViewController.swift
+++ Marathon7thTask/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.contentSize = CGSize(width: view.frame.size.width, height: 1500)
+		
+		scrollView.backgroundColor = .white
+		scrollView.delegate = self
+		scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 270 - view.safeAreaInsets.top, left: 0, bottom: 0, right: 0)
+		
+		return scrollView
+    }()
+    
+	let imageView: UIImageView = {
+		let iv = UIImageView(image: UIImage(named: "monterey.jpg"))
+		iv.contentMode = .scaleAspectFill
+		iv.clipsToBounds = true
+		return iv
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		view.addSubview(scrollView)
+		scrollView.addSubview(imageView)
+		
+		imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 270)
+
+		scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
     }
-
-
 }
 
+extension ViewController: UIScrollViewDelegate {
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+	
+		let offsetY = scrollView.contentOffset.y
+		var width = view.frame.width - offsetY * 2
+		width = max(view.frame.width, width)
+		imageView.frame = CGRect(x: min(0, offsetY), y: min(0, offsetY), width: width, height: 270 - offsetY)
+		
+		if offsetY < 0 {
+			scrollView.scrollIndicatorInsets = UIEdgeInsets(top: imageView.frame.size.height - view.safeAreaInsets.top, left: 0, bottom: 0, right: 0)
+		}
+
+	}
+}
